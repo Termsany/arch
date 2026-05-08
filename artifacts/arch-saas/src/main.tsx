0935ce488123ac8c8a8bc113303a9153ed62522a
Termsany <mostafa.eltermsany@gmail.com>
2026-05-08 15:33:51 +0300

    Wrap app with language provider

--- artifacts/arch-saas/src/main.tsx
+++ artifacts/arch-saas/src/main.tsx
@@ -1,10 +1,15 @@
 import { createRoot } from "react-dom/client";
 import { setAuthTokenGetter } from "@workspace/api-client-react";
 import { configureApiBaseUrl } from "@/lib/api-base";
+import { LanguageProvider } from "@/i18n/language-context";
 import App from "./App";
 import "./index.css";
 
 configureApiBaseUrl();
 setAuthTokenGetter(() => localStorage.getItem("token"));
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <LanguageProvider>
+    <App />
+  </LanguageProvider>
+);
